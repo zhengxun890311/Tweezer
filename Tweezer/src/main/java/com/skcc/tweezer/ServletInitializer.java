ce17d8644160b8dfd221b7a72ff9802ed2ccba71
zhengxun890311 <zhengxun890311@gmail.com>
2019-04-22 14:55:34 -0700

    this is the first comment

---
+++ Tweezer/src/main/java/com/skcc/tweezer/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.skcc.tweezer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TweezerApplication.class);
+	}
+
+}
---
+++ Tweezer/src/main/java/com/skcc/tweezer/TweezerApplication.java
@@ -0,0 +1,13 @@
+package com.skcc.tweezer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweezerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweezerApplication.class, args);
+	}
+
+}
