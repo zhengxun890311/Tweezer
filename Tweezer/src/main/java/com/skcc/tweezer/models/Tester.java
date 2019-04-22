6fe46dbe195e039b175ce8426b30744cb53d074b
zhengxun890311 <zhengxun890311@gmail.com>
2019-04-22 16:42:39 -0700

    hello christian

--- Tweezer/src/main/java/com/skcc/tweezer/models/Tester.java
+++ Tweezer/src/main/java/com/skcc/tweezer/models/Tester.java
@@ -4,7 +4,8 @@ public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
