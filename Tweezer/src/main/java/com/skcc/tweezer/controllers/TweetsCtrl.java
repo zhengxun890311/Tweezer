233d1c6ed3f275b77d58980d0ea6ddadce7ee059
karensitu <karensitu@192.168.0.237>
2019-04-23 13:24:25 -0700

    organized folders

---
+++ Tweezer/src/main/java/com/skcc/tweezer/controllers/TweetsCtrl.java
@@ -0,0 +1,37 @@
+package com.skcc.tweezer.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.skcc.tweezer.models.Tweet;
+import com.skcc.tweezer.models.User;
+import com.skcc.tweezer.services.TweetService;
+
+public class TweetsCtrl {
+	
+	@Autowired
+	private TweetService tweetService;
+	
+	@RequestMapping("/home1")
+	public String home(HttpSession session,@ModelAttribute("tweetObj") Tweet tweet) {
+		System.out.println(session.getAttribute("userId"));
+		return "/home.jsp";
+	}
+	
+	@PostMapping("/createTweet")
+	public String createTweet(HttpSession session,@ModelAttribute("tweetObj") Tweet tweet) {
+		//System.out.println(session.getAttribute("userId"));
+		System.out.println(tweet.getText());
+		System.out.println(tweet.getText());
+		System.out.println(tweet.getText());
+		User newUser = new User();
+		newUser.setId((Long)session.getAttribute("userId"));
+		tweet.setUser(newUser);
+		tweetService.createTweet(tweet);
+		return "/home.jsp";
+	}
+}
--- Tweezer/src/main/java/com/skcc/tweezer/tweet/repository/TweetsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skcc.tweezer.tweet.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.skcc.tweezer.models.Tweet;
-
-public interface TweetsRepository extends CrudRepository<Tweet,Long> {
-	
-}
