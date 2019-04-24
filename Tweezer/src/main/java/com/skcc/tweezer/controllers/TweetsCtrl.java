b34e5ff5061c2a4554be3ef631b4a4f77920bdfc
karensitu <karensitu@192.168.0.237>
2019-04-23 18:57:04 -0700

    fixed create tweet feature

--- Tweezer/src/main/java/com/skcc/tweezer/controllers/TweetsCtrl.java
+++ Tweezer/src/main/java/com/skcc/tweezer/controllers/TweetsCtrl.java
@@ -4,15 +4,16 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skcc.tweezer.models.Tweet;
-import com.skcc.tweezer.models.User;
 import com.skcc.tweezer.services.TweetService;
 
+@Controller
 public class TweetsCtrl {
 	
 	@Autowired
@@ -24,16 +25,22 @@ public class TweetsCtrl {
 		return "/home.jsp";
 	}
 	
+//	@PostMapping("/createTweet")
+//	public String createTweet(HttpSession session,@Valid @ModelAttribute("tweetObj") Tweet tweet, BindingResult result) {
+//		//System.out.println(session.getAttribute("userId"));
+//		System.out.println(tweet.getText());
+//		System.out.println(tweet.getText());
+//		System.out.println(tweet.getText());
+//		User newUser = new User();
+//		newUser.setId((Long)session.getAttribute("userId"));
+//		tweet.setUser(newUser);
+//		tweetService.createTweet(tweet);
+//		return "/home.jsp";
+//	}
+	
 	@PostMapping("/createTweet")
-	public String createTweet(HttpSession session,@Valid @ModelAttribute("tweetObj") Tweet tweet, BindingResult result) {
-		//System.out.println(session.getAttribute("userId"));
-		System.out.println(tweet.getText());
-		System.out.println(tweet.getText());
-		System.out.println(tweet.getText());
-		User newUser = new User();
-		newUser.setId((Long)session.getAttribute("userId"));
-		tweet.setUser(newUser);
+	public String createTweet(HttpSession session, @Valid @ModelAttribute("tweetObj") Tweet tweet, BindingResult result) {
 		tweetService.createTweet(tweet);
-		return "/home.jsp";
+		return "redirect:/home";
 	}
 }
