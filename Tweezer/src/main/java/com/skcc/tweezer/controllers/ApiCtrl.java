a795c3c7762cde1f0e944cc055276a7412a567c9
karensitu <karensitu@192.168.0.237>
2019-04-25 11:06:35 -0700

    changes to the moments jsp and api ctrl

--- Tweezer/src/main/java/com/skcc/tweezer/controllers/ApiCtrl.java
+++ Tweezer/src/main/java/com/skcc/tweezer/controllers/ApiCtrl.java
@@ -23,16 +23,32 @@ public class ApiCtrl {
 
     
     @GetMapping("/news")
-    public String moments(Model model) {
+    public String moments(Model model, HttpSession session) {
     	
+    	Long userId = (Long) session.getAttribute("userId");
+    	if (userId==null) {
+    		return "redirect:/";
+    	} else {
 //		final String uri = "https://content.guardianapis.com/search?q=technology&api-key=c0f4afff-8d9c-4006-8a44-b778650d8e0f";
-    	
+
     	// API for most recent news on Guardian
 		final String uri = "https://content.guardianapis.com/search?api-key=c0f4afff-8d9c-4006-8a44-b778650d8e0f";
 		RestTemplate restTemplate = new RestTemplate();
-		Object result = restTemplate.getForObject(uri,  Object.class);
-		model.addAttribute("news", result);
+		Object news = restTemplate.getForObject(uri,  Object.class);
+		model.addAttribute("news", news);
+		
+		final String uri2 = "https://content.guardianapis.com/sections?api-key=c0f4afff-8d9c-4006-8a44-b778650d8e0f";
+		RestTemplate restTemplate2 = new RestTemplate();
+		Object trends = restTemplate2.getForObject(uri2,  Object.class);
+		model.addAttribute("trends", trends);
+		
+		User u = uS.findUserById(userId);
+		model.addAttribute("user", u);
+		model.addAttribute("loggedUser", uS.findUserById(userId));
+		model.addAttribute("whoToFollow", uS.getWhoToFollow(userId));
+		
     	return "moments.jsp";
+    	}
     }
     
     
