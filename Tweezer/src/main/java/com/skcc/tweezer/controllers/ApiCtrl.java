d481254761c7f969c254bef19f99963484984444
karensitu <karensitu@192.168.0.237>
2019-04-25 22:03:47 -0700

    tested tweetfeed query on api jsp/ctrl. move changes to home jsp tmr

--- Tweezer/src/main/java/com/skcc/tweezer/controllers/ApiCtrl.java
+++ Tweezer/src/main/java/com/skcc/tweezer/controllers/ApiCtrl.java
@@ -1,5 +1,8 @@
 package com.skcc.tweezer.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.client.RestTemplate;
 
+import com.skcc.tweezer.models.Like;
 import com.skcc.tweezer.models.Reply;
 import com.skcc.tweezer.models.Tweet;
 import com.skcc.tweezer.models.User;
@@ -51,5 +55,49 @@ public class ApiCtrl {
     	}
     }
     
+    @GetMapping("/test")
+    public String home(@ModelAttribute("tweetObj") Tweet tweet, Model model, @ModelAttribute("replyObj") Reply reply, @ModelAttribute("followUserObj") User followUser, @ModelAttribute("likeObj") Like like, @ModelAttribute("unlikeObj") Like unlike, HttpSession session) {
+    	Long userId = (Long) session.getAttribute("userId");
+    	if (userId==null) {
+    		return "redirect:/";
+    	}else {
+
+    		final String uri = "https://content.guardianapis.com/sections?api-key=c0f4afff-8d9c-4006-8a44-b778650d8e0f";
+    		RestTemplate restTemplate = new RestTemplate();
+    		Object result = restTemplate.getForObject(uri,  Object.class);
+    	
+    		User u = uS.findUserById(userId);
+    		model.addAttribute("user", u);
+    		model.addAttribute("following", u.getUserFollowing());
+    		model.addAttribute("loggedUser", uS.findUserById(userId));
+    		model.addAttribute("followingTweets", uS.getFollowingTweets(userId));
+    		model.addAttribute("whoToFollow", uS.getWhoToFollow(userId));
+    		model.addAttribute("trends", result);
+    		
+    		List<Number> following = new ArrayList<Number>();
+    		
+    		String str = "";
+    		
+    		List<Object[]> res = uS.getFollowing(userId);
+    		res.forEach(i -> {
+    			following.add((Number) i[0]);
+    		});
+    		
+    		System.out.println(following);
+    		
+    		for (int i=0; i<following.size(); i++) {
+    			str += following.get(i);
+    		}
+    		
+    		System.out.println(str);
+    		
+    		model.addAttribute("follow", following);
+    		model.addAttribute("str", str);
+    		
+    		model.addAttribute("allTweets", uS.getAllTweets());
+    		
+    		return "api.jsp";    		   		
+    	}
+    }
     
 }
